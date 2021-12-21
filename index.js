572c7660cd30e527868c4dd1f1879f72068bf43f
Seth Morenos <sethvm64@gmail.com>
2021-12-21 03:48:43 -0500

    implement /random endpoint

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const quotes = require('./data/quotes.json')
 
+/* GET ENDPOINTS */
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('kimi.rest API')
 })
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+app.get('/random', (req, res) => {
+    res.send(selectRandomQuote())
 })
+
+/* PORT */
+app.listen(
+    port,
+    () => console.log(`Live at http://localhost:${port}`)
+)
+
+/* FUNCTIONS */
+function selectRandomQuote() {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    return JSON.stringify(quote);
+}
