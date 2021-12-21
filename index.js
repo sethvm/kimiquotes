9457f9a1ebbea6f53b49370d0113d3f70d60430a
Seth Morenos <sethvm64@gmail.com>
2021-12-21 13:14:43 -0500

    implement new endpoints, restructure quotes data

--- index.js
+++ index.js
@@ -5,21 +5,40 @@ const quotes = require('./data/quotes.json')
 
 /* GET ENDPOINTS */
 app.get('/', (req, res) => {
-    res.send('kimi.rest API')
+    res.send('GET request at /')
 })
 
-app.get('/random', (req, res) => {
+app.get('/all', (req, res) => {
+    res.send(displayAllQuotes())
+})
+
+app.get('/random', (req,res) => {
     res.send(selectRandomQuote())
 })
+
+app.get('/index/:index', (req, res) => {
+    const { index } = req.params;
+    res.send(selectQuoteByIndex(index))
+})
+
+/* FUNCTIONS */
+function displayAllQuotes() {
+    return quotes;
+}
+
+function selectRandomQuote() {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    return quote;
+}
+
+function selectQuoteByIndex(index) {
+    if (!quotes[index]) return `404: no quote found`;
+    const quote = quotes[index];
+    return quote;
+}
 
 /* PORT */
 app.listen(
     port,
     () => console.log(`Live at http://localhost:${port}`)
 )
-
-/* FUNCTIONS */
-function selectRandomQuote() {
-    const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    return JSON.stringify(quote);
-}
