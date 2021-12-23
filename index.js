38a7c06ea89b44805d39ed5dcf69f28fd4ac0fc3
Seth Morenos <sethvm64@gmail.com>
2021-12-23 18:09:54 -0500

    fix description, add to documentation

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(helmet())
 app.use(compression())
 app.use(express.static('client'))
 
-// GET ENDPOINTS
+// ROUTES
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './client/index.html'));
 })
