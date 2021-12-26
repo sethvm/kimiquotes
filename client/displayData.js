5b409ec27aad88a4bbdd8bd20124b927c9592945
Seth Morenos <sethvm64@gmail.com>
2021-12-25 21:12:35 -0500

    clean up quote rendering, update footer

--- client/displayData.js
+++ client/displayData.js
@@ -1,6 +1,5 @@
 // document selectors
 const sampleQuote = document.getElementById('sampleQuote');
-const sampleQuoteDate = document.getElementById('quoteDate');
 const quoteResetBtn = document.getElementById('quoteResetBtn');
 const indexRange = document.getElementById('indexRange');
 
@@ -19,13 +18,12 @@ async function renderSampleQuote() {
     const quoteData = await response.json();
 
     if (quoteData.year === null) quoteData.year = '';
-    sampleQuote.textContent = `"${quoteData.quote}"`;
-    sampleQuoteDate.textContent = `- Kimi Räikkönen${quoteData.year && `, ${quoteData.year}`}`;
+    sampleQuote.innerText = `"${quoteData.quote}"\n\n- Kimi Räikkönen${quoteData.year && `, ${quoteData.year}`}`;
 }
 
 async function renderQuoteIndexRange() {
     const response = await fetch(`${window.location.href}quotes`);
     const quoteData = await response.json();
 
-    indexRange.textContent = ` (any value from 0 to ${quoteData.length - 1})`;
+    indexRange.innerText = ` (any value from 0 to ${quoteData.length - 1})`;
 }
