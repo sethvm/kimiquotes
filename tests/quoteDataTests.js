d8581eaadbae7ad7cda97dd6a9fd5f2660d82aba
Seth Morenos <sethvm64@gmail.com>
2021-12-30 16:18:06 -0500

    integrate testing library, update quotes

---
+++ tests/quoteDataTests.js
@@ -0,0 +1,26 @@
+// returns true if all quote objects have the required properties (id, quote)
+// returns index of object with missing properties on test fail
+function quotePropertiesValid(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].id || !arr[i].quote) {
+            return i;
+        }
+    }
+    return true;
+}
+
+// returns true if quote IDs are properly sequenced (ID = index + 1)
+// returns index of object containing mismatch on test fail
+function quoteIdsValid(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].id !== i + 1) {
+            return i;
+        }
+    }
+    return true;
+}
+
+module.exports = {
+    quotePropertiesValid,
+    quoteIdsValid
+}
