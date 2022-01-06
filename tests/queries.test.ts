6dae61fe2e24072c6e82372be04f4a3e572958f9
Seth Morenos <sethvm64@gmail.com>
2022-01-06 00:16:49 -0500

    create boilerplate for query testing

---
+++ tests/queries.test.ts
@@ -0,0 +1,21 @@
+/* URIs TO TEST
+
+    /quotes
+
+    /quotes/unstamped
+
+    /quotes/hello
+
+    /quotes/2012
+
+    /quotes/2022
+
+    /quotes/-2012
+
+    /quote/42
+
+    /quote/420
+
+    /quote/-42
+
+*/
